bfb5702a065fce53c86f5c42c232a877d2534cce
roblowe84 <81348921+roblowe84@users.noreply.github.com>
2026-05-11 12:23:49 +0000

    Labsheet1_Questions2Completed

--- main.sql
+++ main.sql
@@ -42,3 +42,20 @@ SELECT name, birth, species FROM pet WHERE species = 'cat' OR species = 'bird';
 -- Q1-6 The names and species of pets which are cats or birds and which are female
 SELECT name, species, sex FROM pet WHERE (species = 'cat' OR species = 'bird') AND sex = 'f';
 
+-- Q2-1 The names of owners and their pets where the pet's name ends with "er" or "all"
+SELECT owner, name FROM pet WHERE name LIKE '%er' OR name LIKE '%all';
+
+-- Q2-2 The names of any pets whose owner's name contains an "e"
+SELECT name, owner FROM pet WHERE owner LIKE '%e%';
+
+-- Q2-3 The names of all pets whose name does not end with "fy"
+SELECT name FROM pet WHERE name NOT LIKE '%fy';
+
+-- Q2-4 All pet names whose owners name is only four characters long
+SELECT name, owner FROM pet WHERE LENGTH(owner)=4;
+
+-- Q2-5 All owners whose names begin and end with one of the first five letters of the alphabet
+SELECT DISTINCT owner FROM pet WHERE UPPER(owner) GLOB '[A-E]*[A-E]';
+
+-- Q2-6 Repeat the previous query, but make the query sensitive to the case of letters of the alphabet the characters in the name
+SELECT DISTINCT owner FROM pet WHERE UPPER(owner) GLOB '[A-E]*[A-E]';
